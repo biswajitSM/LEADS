ef50b2dabdcc6e83a3475c1a38f643f147f0c59a
Biswajit Pradhan <biswajitpradha@tudelft.net>
2020-08-23 20:47:01 +0200

    installation requrements added to setup

--- setup.py
+++ setup.py
@@ -17,5 +17,21 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent"
     ],
-    python_requires = '>=3.8'
+    python_requires = '>=3.8',
+    install_requires = [
+        "numpy>=1.18",
+        "pandas==1.0.3",
+        "scipy>=1.4",
+        "scikit-image>=0.17",
+        "PyQt5==5.15.0",
+        "napari==0.3.4",
+        "pyqtgraph>=0.11",
+        "dask>=2.16.0",
+        "PySimpleGUI",
+        "pims==0.4.1",
+        "tqdm==4.46.1",
+        "tifffile==2020.6.3",
+        "roifile==2020.5.28",
+        "PyYAML"
+    ]
 )
