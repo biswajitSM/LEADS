16ecada23557122e54c42e8f663c24bc33cf22f1
Biswajit Pradhan <biswajitp145@gmail.com>
2021-01-26 10:26:31 +0100

    matplots labelsizes of axes and ticks

--- leads/utils/figure_params.py
+++ leads/utils/figure_params.py
@@ -3,6 +3,9 @@ params_dict = {
     'axes.labelsize': 'medium',
     'xtick.top' : True,
     'ytick.right' : True,
+    'axes.labelsize' : 16,
+    'xtick.labelsize' : 12,
+    'ytick.labelsize' : 12,
     'svg.fonttype' : 'none',
     'pdf.fonttype' : 42,
     'savefig.transparent' : True
