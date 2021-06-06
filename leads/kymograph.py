f10082fb02d3e377885135185e7affc52217d9b7
Biswajit Pradhan <biswajitp145@gmail.com>
2021-06-06 13:47:51 +0200

    histogram share y axis

--- leads/kymograph.py
+++ leads/kymograph.py
@@ -298,13 +298,13 @@ def link_and_plot_two_color(df_peaks, df_peaks_sm,
     ax3 = fig.add_subplot(gs[2, :-1])
     ax4 = fig.add_subplot(gs[3, :-1])
     # add axs for histogram
-    ax1_r = fig.add_subplot(gs[0, -1:])
+    ax1_r = fig.add_subplot(gs[0, -1:], sharey=ax1)
     ax1_r.set_xticks([])
     ax1_r.set_yticks([])
-    ax2_r = fig.add_subplot(gs[1, -1:])
+    ax2_r = fig.add_subplot(gs[1, -1:], sharey=ax2)
     ax2_r.set_xticks([])
     ax2_r.set_yticks([])
-    ax3_r = fig.add_subplot(gs[2, -1:])
+    ax3_r = fig.add_subplot(gs[2, -1:], sharey=ax3)
     ax3_r.set_xticks([])
     ax3_r.set_yticks([])
     # link and plot data to it
